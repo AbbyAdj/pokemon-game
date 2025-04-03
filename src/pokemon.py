f9585abb1b9c4b1a515475c6336cac9ec90bc84e
AbbyAdj <abbyadjei9@gmail.com>
2025-04-03 11:48:59 +0100

    complete tasks 1-3

---
+++ src/pokemon.py
@@ -0,0 +1,66 @@
+
+class Pokemon:
+    def __init__(self, 
+                 name:str,
+                 hitpoints:int,
+                 attack_damage:int, 
+                 move:str):
+        self.name = name
+        self.hitpoints = hitpoints
+        self.attack_damage = attack_damage
+        self.move = move
+        # TODO change self.health to private and refactor
+        self.health = 100
+        # TODO change strong against and weak against to private
+        self.strong_against = []
+        self.weak_against = []
+
+    def use_move(self)-> str:
+        return f"{self.name} used {self.name}'s move"
+
+    def take_damage(self, amount:int):
+        self.health -= amount
+
+    def has_fainted(self) -> bool:
+        """Returns true if health has hit zero and false otherwise"""
+        if self.health == 0:
+            return True
+        return False
+
+    def get_multiplier(self, pokemon) -> float:
+        """Returns 1.5 if pokemon in weak against list
+        Returns 0.5 if pokemon in strong against list
+        Returns 1 if pokemon isn't found in either list"""
+        if pokemon.type in self.strong_against:
+            return 1.5
+        if pokemon.type in self.weak_against:
+            return 0.5
+        return 1
+
+
+class FirePokemon(Pokemon):
+    def __init__(self, name, hitpoints, attack_damage, move):
+        super().__init__(name, hitpoints, attack_damage, move)
+        self.type = "fire"
+        self.strong_against = ["grass"]
+        self.weak_against = ["water"]
+
+class WaterPokemon(Pokemon):
+    def __init__(self, name, hitpoints, attack_damage, move):
+        super().__init__(name, hitpoints, attack_damage, move)
+        self.type = "water"
+        self.strong_against = ["fire"]
+        self.weak_against = ["grass"]
+
+class GrassPokemon(Pokemon):
+    def __init__(self, name, hitpoints, attack_damage, move):
+        super().__init__(name, hitpoints, attack_damage, move)
+        self.type = "grass"
+        self.strong_against = ["water"]
+        self.weak_against = ["fire"]
+
+class NormalPokemon(Pokemon):
+    def __init__(self, name, hitpoints, attack_damage, move):
+        super().__init__(name, hitpoints, attack_damage, move)
+        self.type = "normal"
+
---
+++ test/test_pokemon.py
@@ -0,0 +1,130 @@
+from src.pokemon import Pokemon, FirePokemon, WaterPokemon, GrassPokemon, NormalPokemon
+import pytest
+
+class TestParentProperties:
+    def test_initialises_with_attributes(self):
+        pokemon = Pokemon(name="Eevee",
+                          hitpoints=55,
+                          attack_damage=18,
+                          move="Headbutt")
+        
+        assert pokemon.name == "Eevee"
+        assert pokemon.hitpoints == 55
+        assert pokemon.attack_damage == 18
+        assert pokemon.move == "Headbutt"
+
+class TestChildProperties:
+    def test_fire_pokemon(self):
+        fire_pokemon = FirePokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+
+        assert fire_pokemon.type == "fire"
+        assert fire_pokemon.name == "Eevee"
+        assert fire_pokemon.strong_against == ["grass"]
+        assert fire_pokemon.weak_against == ["water"]
+
+    def test_water_pokemon(self):
+        fire_pokemon = WaterPokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+
+        assert fire_pokemon.type == "water"
+        assert fire_pokemon.name == "Eevee"
+        assert fire_pokemon.strong_against == ["fire"]
+        assert fire_pokemon.weak_against == ["grass"]
+
+    def test_grass_pokemon(self):
+        fire_pokemon = GrassPokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+
+        assert fire_pokemon.type == "grass"
+        assert fire_pokemon.name == "Eevee"
+        assert fire_pokemon.strong_against == ["water"]
+        assert fire_pokemon.weak_against == ["fire"]
+
+    def test_normal_pokemon(self):
+        fire_pokemon = NormalPokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+
+        assert fire_pokemon.type == "normal"
+        assert fire_pokemon.name == "Eevee"
+        assert fire_pokemon.strong_against == []
+        assert fire_pokemon.weak_against == []
+
+class TestUseMoveMethod:
+    def test_use_move_returns_string(self):
+        pokemon = Pokemon(name="Eevee",
+                          hitpoints=55,
+                          attack_damage=18,
+                          move="Headbutt")
+        
+        assert pokemon.use_move() == "Eevee used Eevee's move"
+
+class TestTakeDamageMethod:
+    def test_take_damage_reduces_health(self):
+        pokemon = Pokemon(name="Eevee",
+                          hitpoints=55,
+                          attack_damage=18,
+                          move="Headbutt")
+        pokemon.take_damage(20) 
+        assert pokemon.health == 80          
+        pokemon.take_damage(20) 
+        assert pokemon.health == 60   
+
+class TestHasFaintedMethod:
+    def test_for_health_above_zero(self):
+        pokemon = Pokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+        
+        assert pokemon.has_fainted() is False
+
+    def test_for_health_at_zero(self):
+        pokemon = Pokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+        
+        pokemon.take_damage(100)
+
+        assert pokemon.has_fainted() is True
+
+class TestGetMultiplierMethod:
+    def test_get_multiplier_method(self):
+        
+        fire = FirePokemon(name="Flareon",
+                           hitpoints=65,
+                           attack_damage=20,
+                           move="Fire blast")
+        
+        grass = GrassPokemon(name="Leafeon",
+                             hitpoints=65,
+                             attack_damage=17,
+                             move="Giga drain")
+        
+        normal = NormalPokemon(name="Eevee",
+                        hitpoints=55,
+                        attack_damage=18,
+                        move="Headbutt")
+        
+        water = WaterPokemon(name="Vaporeon",
+                        hitpoints=70,
+                        attack_damage=19,
+                        move="Hydro pump")
+        
+        multiplier_1_point_5 = fire.get_multiplier(pokemon=grass)
+        multiplier_1= fire.get_multiplier(pokemon=normal)
+        multiplier_0_point_5 = fire.get_multiplier(pokemon=water)
+
+        assert multiplier_1_point_5 == 1.5
+        assert multiplier_1 == 1
+        assert multiplier_0_point_5 == 0.5
+

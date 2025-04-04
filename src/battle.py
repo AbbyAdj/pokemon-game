5e1b50d744c0d88b95c958b90ce141a1e0488383
AbbyAdj <abbyadjei9@gmail.com>
2025-04-04 11:03:19 +0100

    complete challenge 6

--- src/battle.py
+++ src/battle.py
@@ -1,30 +1,89 @@
+from src.pokemon import FirePokemon, GrassPokemon
+
+class PokemonHasFainted(Exception):
+    pass
+
 
 class Battle:
     def __init__(self,pokemon_1,pokemon_2):
+        """Takes in two pokemon as arguments to start a battle between them"""
         self.pokemon_1 = pokemon_1
         self.pokemon_2 = pokemon_2
-        self.current_player = self.pokemon_1
+        # might need to refactor and use self.pokemon1 and self.pokemon2 directly.
+        self.attacker = self.pokemon_1
+        self.defender = self.pokemon_2
 
     def pokemon_1_turn(self):
-        self.current_player = self.pokemon_2
+        """Invokes pokemon 1's turn and has it inflict damage on pokemon 2
+        Turns switch at the end of invocation"""
+        self.calculate_damage()
+        # at the end of the turn........
+        self.attacker = self.pokemon_2
+        self.defender = self.pokemon_1
 
     def pokemon_2_turn(self):
-        self.current_player = self.pokemon_1
+        """Invokes pokemon 2's turn and has it inflict damage on pokemon 1
+        Turns switch at the end of invocation"""
+        self.calculate_damage()
+        # at the end of the turn........
+        self.attacker = self.pokemon_1
+        self.defender = self.pokemon_2
 
-    def calculate_damage(self, attacker, defender):
-        pass
+    def calculate_damage(self):
+        """Calculates the total damage the attack deals on the defender's hitpoints"""
+        # We might need to take out the two parameters above and use the self ones directly
+        # call take damage for the defender. 
+        # Use the attackers multiplier to check the total damage the attacker will infict
+        
+        total_attacker_damage = self.attacker.attack_damage * self.attacker.get_multiplier(self.defender)
+        self.defender.take_damage(amount=total_attacker_damage)
+
+        # Call this method at the beginning of each pokemon's turn method, 
 
     def take_turn(self):
-        if self.current_player == self.pokemon_1:
+        """Assigns and manages the turns per round and raises an exception if a pokemon has fainted"""
+        if self.attacker == self.pokemon_1:
             self.pokemon_1_turn()
-            return
-        if self.current_player == self.pokemon_2:
+        elif self.attacker == self.pokemon_2:
             self.pokemon_2_turn()
-            return
-        raise Exception("Pokemon(insert name) has fainted. No more turns")
+        if self.pokemon_1.has_fainted():
+            raise PokemonHasFainted(f"{self.pokemon_1.name} has fainted. No more turns")        
+        if self.pokemon_2.has_fainted():
+            raise PokemonHasFainted(f"{self.pokemon_2.name} has fainted. No more turns")
         
-        
-    
     def get_winner(self):
-        pass
+        """Returns winner if other pokemon has fainted"""
+        if self.pokemon_1.has_fainted():
+            return self.pokemon_2
+        if self.pokemon_2.has_fainted():
+            return self.pokemon_1
+        return None
 
+
+
+
+
+
+
+if __name__ == "__main__":
+    fire_pokemon = FirePokemon(name="Flareon",
+                    hitpoints=65,
+                    attack_damage=20,
+                    move="Fire blast")
+    grass_pokemon = GrassPokemon(name="Leafeon",
+                    hitpoints=65,
+                    attack_damage=17,
+                    move="Giga drain")
+    battle = Battle(pokemon_1 = fire_pokemon ,pokemon_2 = grass_pokemon) 
+    battle.take_turn()
+    print(battle.pokemon_2.hitpoints)
+
+    battle.take_turn() 
+    battle.take_turn() 
+    print(battle.pokemon_2.hitpoints)
+    battle.take_turn() 
+    battle.take_turn() 
+    print(battle.pokemon_2.hitpoints)
+
+
+    
--- test/test_battle.py
+++ test/test_battle.py
@@ -1,4 +1,4 @@
-from src.battle import Battle
+from src.battle import Battle, PokemonHasFainted
 import pytest
 
 
@@ -13,18 +13,79 @@ class TestBattleTakeTurnMethod:
     def test_switches_attacker_and_defender_after_each_turn(self,fire_pokemon, grass_pokemon):
         battle = Battle(pokemon_1 = fire_pokemon ,pokemon_2 = grass_pokemon)
 
-        assert battle.current_player == fire_pokemon
+        assert battle.attacker == fire_pokemon
+        assert battle.defender == grass_pokemon
 
         battle.take_turn()
-        assert battle.current_player == grass_pokemon
-        
+
+        assert battle.attacker == grass_pokemon
+        assert battle.defender == fire_pokemon
+                  
+    def test_method_calculates_damage_to_the_opponent_and_subtracts_from_HP(self, fire_pokemon, grass_pokemon):
+        battle = Battle(pokemon_1 = fire_pokemon ,pokemon_2 = grass_pokemon)
+        # fire hp - 65
+        # fire attack damage 20
+        # fire multiplier 1.5
+        # fire total damage = 30
+
+        # grass hp 65
+        # grass attack damage 17
+        # grass multiplier 0.5
+        # grass total damage 8.5
+
+        # Beginning of the battle
+
+        assert battle.pokemon_1.hitpoints == 65
+        assert battle.pokemon_2.hitpoints == 65
+
+        # round 1 - attacker : fire | defender : grass
+        battle.pokemon_1_turn()
+
+        assert battle.pokemon_1.hitpoints == 65
+        assert battle.pokemon_2.hitpoints == 35
+
+        # # round 2 - attacker : grass | defender : fire
+        battle.pokemon_2_turn()
+
+        assert battle.pokemon_1.hitpoints == 56.5
+        assert battle.pokemon_2.hitpoints == 35
+
+        # # round 3 - attacker : fire | defender : grass
+        battle.pokemon_1_turn()
+
+        assert battle.pokemon_1.hitpoints == 56.5
+        assert battle.pokemon_2.hitpoints == 5
+
+    def test_raises_exception_if_a_pokemon_has_fainted(self, fire_pokemon, grass_pokemon):
+        battle = Battle(pokemon_1 = fire_pokemon ,pokemon_2 = grass_pokemon)
+
+        # TODO use a while loop or a for loop for the take turns below:
+
+        battle.take_turn() #1st round : fire attacks
+        battle.take_turn() #2nd round : grass attacks
+        battle.take_turn() #3rd round : fire attacks
+        battle.take_turn() #4th round : grass attacks
+        with pytest.raises(PokemonHasFainted) as err:
+            battle.take_turn() #5th round : fire attacks (grass faints)
+        assert str(err.value) == "Leafeon has fainted. No more turns"
+
+class TestGetWinnerMethod:
+    def test_returns_none_if_no_pokemon_has_fainted(self, fire_pokemon, grass_pokemon):
+        battle = Battle(pokemon_1 = fire_pokemon ,pokemon_2 = grass_pokemon)
+
         battle.take_turn()
-        assert battle.current_player == fire_pokemon
-            
 
-    def test_method_calculates_damage_to_the_opponent_and_subtracts_from_HP(self):
-        pass
-    
-    def test_raises_exception_if_a_pokemon_has_fainted(self):
-        pass
+        assert battle.get_winner() is None
 
+    def test_returns_winner_if_one_pokemon_has_fainted(self, fire_pokemon, grass_pokemon):
+        battle = Battle(pokemon_1 = fire_pokemon ,pokemon_2 = grass_pokemon)
+        try:
+            battle.take_turn()
+            battle.take_turn()
+            battle.take_turn()
+            battle.take_turn()
+            battle.take_turn()
+        except PokemonHasFainted:
+            print("The fight ended.......checking winner")
+        else:
+            assert battle.get_winner() == grass_pokemon

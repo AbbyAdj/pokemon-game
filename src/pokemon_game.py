2dd35a236dc3b11af085bc075f3e2c25966007e5
AbbyAdj <abbyadjei9@gmail.com>
2025-04-04 15:12:50 +0100

    create pokemon game class and test

---
+++ src/pokemon_game.py
@@ -0,0 +1,51 @@
+from trainer import Trainer
+from battle import Battle, PokemonHasFainted
+import random
+
+class PokemonGame:
+    def __init__(self, trainer_one, trainer_two):
+        self.trainer_one = trainer_one
+        self.trainer_two = trainer_two
+
+        self.player_one = self.trainer_one
+        self.player_two = self.trainer_two
+
+    def decide_starting_player(self):
+        first_player_decision = random.randint(1,2)
+        if first_player_decision == 2:
+            self.player_one = self.trainer_two
+            self.player_two = self.trainer_one
+    
+    def player_catch_pokemon(self, first_pokemon, second_pokemon) -> bool:
+        self.player_one.throw_pokeball(first_pokemon)
+        self.player_two.throw_pokeball(second_pokemon)
+    
+    def start_battle(self):
+        player_one_pokemon = self.player_one.belt[0].pokemon
+        player_two_pokemon = self.player_two.belt[0].pokemon
+
+        game = Battle(pokemon_1=player_one_pokemon,
+                      pokemon_2=player_two_pokemon)
+        
+        game_ongoing = True
+        
+        while game_ongoing:
+            try:
+                game.take_turn()
+            except PokemonHasFainted:
+                winner = game.get_winner()
+                return winner
+                # game_ongoing = False
+            else:
+                pass
+                # communicate trainer who won
+    
+    def show_winner(self):
+        winner_pokemon = self.start_battle()
+        winner = self.player_one
+
+        if self.player_one.belt[0].pokemon.is_fainted():
+            winner = self.player_two
+
+        print(f"{winner_pokemon} pokemon has won. {winner.name} is the winner.")
+        
---
+++ test/test_pokemon_game.py
@@ -0,0 +1,8 @@
+from src.pokemon_game import PokemonGame
+import pytest
+
+
+class Test:
+    def test(self):
+        pass
+
